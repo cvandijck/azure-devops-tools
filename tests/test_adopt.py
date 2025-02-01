98a3d9a7bee7235aa42683d9f8f68dd6373b18c5
Christophe Van Dijck <christophe.vdijck@gmail.com>
2025-02-01 13:36:36 +0100

    fix tests

--- tests/test_adopt.py
+++ tests/test_adopt.py
@@ -27,12 +27,3 @@ def test_console_help():
     """Calls help file of console script and tests for failure."""
     process = subprocess.run([PYTHON_EXE, '-m', 'adopt', '--help'], capture_output=True, universal_newlines=True)
     assert process.returncode == 0, process.stderr
-
-
-@pytest.mark.console
-def test_console_noargs():
-    """Runs console without arguments expecting to fail."""
-    process = subprocess.run(
-        [PYTHON_EXE, '-m', 'adopt', 'backlog', 'sort'], capture_output=True, universal_newlines=True
-    )
-    assert process.returncode == 1, process.stderr
