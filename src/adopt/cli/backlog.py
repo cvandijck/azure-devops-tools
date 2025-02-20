27ba7eabf3c2b236070897fc3f80f3556cea2490
Christophe Van Dijck <christophe.vdijck@gmail.com>
2025-02-20 21:41:27 +0100

    fix input validation

--- src/adopt/cli/backlog.py
+++ src/adopt/cli/backlog.py
@@ -1,7 +1,7 @@
 import click
 
 from adopt.backlog.sort import DEFAULT_SORT_KEY, VALID_SORT_KEY_STR, sort_backlog
-from adopt.cli import log_option, project_option, team_option, token_option, url_option
+from adopt.cli import CONTEXT_SETTINGS, log_option, project_option, team_option, token_option, url_option
 from adopt.connect import create_connection, get_work_client, get_work_item_tracking_client
 from adopt.formatting import format_backlog_as_table
 from adopt.logging import configure_logging, convert_logging_level
@@ -13,17 +13,19 @@ def cli_backlog(): ...
 
 
 category_type = click.Choice(['Story', 'Feature', 'Epic'], case_sensitive=False)
-category_option = lambda: click.option('--category', '-c', help='Backlog Category', default='Story', type=category_type)
+category_option = lambda: click.option(
+    '--category', '-c', help='Backlog Category', default='Story', type=category_type, required=True
+)
 
 
-@cli_backlog.command(name='sort', help='Sort the backlog')
+@cli_backlog.command(name='sort', help='Sort the backlog', context_settings=CONTEXT_SETTINGS)
 @url_option()
 @token_option()
 @project_option()
 @team_option()
 @log_option()
 @category_option()
-@click.option('--sort_key', '-s', help=f'Sort key [{VALID_SORT_KEY_STR}]', default=DEFAULT_SORT_KEY)
+@click.option('--sort_key', '-s', help=f'Sort key [{VALID_SORT_KEY_STR}]', default=DEFAULT_SORT_KEY, required=True)
 def cli_sort_backlog(url: str, token: str, project: str, team: str, category: str, sort_key: str, log_level: str):
     log_level = convert_logging_level(log_level)
     configure_logging(level=log_level, exclude_external_logs=True)
@@ -43,7 +45,7 @@ def cli_sort_backlog(url: str, token: str, project: str, team: str, category: st
     )
 
 
-@cli_backlog.command(name='print', help='Print the backlog')
+@cli_backlog.command(name='print', help='Print the backlog', context_settings=CONTEXT_SETTINGS)
 @url_option()
 @token_option()
 @project_option()
