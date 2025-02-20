65071c1235949cbb572bdb272a333d6ace13c46f
Christophe Van Dijck <christophe.vdijck@gmail.com>
2025-02-20 21:05:38 +0100

    refactor config loading

--- src/adopt/cli/__init__.py
+++ src/adopt/cli/__init__.py
@@ -1,13 +1,13 @@
 import click
 
-from adopt.env import PAT_ENV, PROJECT_ENV, TEAM_ENV, URL_ENV
+from adopt.config import ADO_ORG_URL_VAR, ADO_PAT_VAR, ADO_PROJECT_VAR, ADO_TEAM_VAR
 
 log_type = click.Choice(['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], case_sensitive=False)
 
-url_option = lambda: click.option('--url', '-u', help='Organization URL', envvar=URL_ENV)
-token_option = lambda: click.option('--token', '-t', help='Personal Access Token', envvar=PAT_ENV)
-project_option = lambda: click.option('--project', '-p', help='Project Name', envvar=PROJECT_ENV)
-team_option = lambda: click.option('--team', '-t', help='Team Name', envvar=TEAM_ENV)
+url_option = lambda: click.option('--url', '-u', help='Organization URL', envvar=ADO_ORG_URL_VAR)
+token_option = lambda: click.option('--token', '-t', help='Personal Access Token', envvar=ADO_PAT_VAR)
+project_option = lambda: click.option('--project', '-p', help='Project Name', envvar=ADO_PROJECT_VAR)
+team_option = lambda: click.option('--team', '-t', help='Team Name', envvar=ADO_TEAM_VAR)
 log_option = lambda: click.option('--log-level', '-l', help='Log Level', default='INFO', type=log_type)
 
 
