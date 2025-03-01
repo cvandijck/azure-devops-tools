2e1b6cee79fee4047b9e72e0f7581372b1660e90
Christophe Van Dijck <christophe.vdijck@gmail.com>
2025-03-01 20:08:00 +0100

    add fix operation

--- src/adopt/__main__.py
+++ src/adopt/__main__.py
@@ -1,7 +1,6 @@
 import logging
 
 from adopt.cli import cli_root
-from adopt.cli.backlog import cli_backlog
 from adopt.config import initialize_configuration
 
 LOGGER = logging.getLogger(__name__)
@@ -9,7 +8,6 @@ LOGGER = logging.getLogger(__name__)
 
 def main():
     initialize_configuration()
-    cli_root.add_command(cli_backlog)
     cli_root()
 
 
